bf891a78e279fad9b7eb16f7eba1842963742570
Evann DREUMONT <53308142+LeGmask@users.noreply.github.com>
2025-01-08 18:41:57 +0100

    fix: refactor Client + support for adding files at startup

--- app/src/main/java/n7/HagiMule/Client/Client.java
+++ app/src/main/java/n7/HagiMule/Client/Client.java
@@ -1,50 +1,40 @@
 package n7.HagiMule.Client;
 
 import java.io.IOException;
-import java.net.ConnectException;
 import java.net.MalformedURLException;
 import java.rmi.Naming;
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 import n7.HagiMule.Diary.Diary;
 
-public class Client {
+// catch (ConnectException e) {
+// System.out.println("Connexion refusée. Est-ce que le serveur est allumé et joingable ?");}
 
-    public static void printHelp() {
-        System.out.println("Usage : java Client <diary address> <diary port>");
+public class Client {
+    Diary index;
+    DaemonImpl daemon;
+    DownloaderImpl downloader;
+
+    public Client(Boolean tui, String host, String port, String[] files) {
+        initializeComponents(host, port);
+        registerHooks();
+        startComponents();
+
+        registerInitialFiles(files);
+
+        if (tui)
+            try {
+                new Tui(index, daemon, downloader);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
     }
 
-    public static void main(String[] args) throws IOException {
-        if (args.length < 2) {
-            printHelp();
-            System.exit(1);
-        }
-
-        String diaryAdress = String.format("//%s:%s/Diary", args[0], args[1]);
+    private void initializeComponents(String host, String port) {
         try {
-            // reaching out to the Diary
-            Diary index = (Diary) Naming.lookup(diaryAdress);
-            // launching the Daemon
-            DaemonImpl daemon = new DaemonImpl(index);
-            daemon.start();
-
-            DownloaderImpl downloader = new DownloaderImpl(index);
-
-            // adding a shutdown hook to unregister the Daemon and free the port
-            Runtime.getRuntime()
-                    .addShutdownHook(
-                            new Thread() {
-                                public void run() {
-                                    try {
-                                        daemon.close();
-                                    } catch (IOException e) {
-                                        e.printStackTrace();
-                                    }
-                                }
-                            });
-
-            // Start the TUI
-            Tui tui = new Tui(index, daemon, downloader);
+            index = (Diary) Naming.lookup(String.format("//%s:%s/Diary", host, port));
+            daemon = new DaemonImpl(index);
+            downloader = new DownloaderImpl(index);
         } catch (MalformedURLException e) {
             System.out.println("The RMI registry is incorrect.");
             e.printStackTrace();
@@ -53,11 +43,71 @@ public class Client {
             e.printStackTrace();
         } catch (RemoteException e) {
             System.out.println("Cannot reach the RMI registry : " + e.getLocalizedMessage());
-        } catch (ConnectException e) {
-            System.out.println(
-                    "Connexion refusée. Est-ce que le serveur est allumé et joingable ?");
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    private void registerHooks() {
+        // shutdown hook for daemon
+        Runtime.getRuntime()
+                .addShutdownHook(
+                        new Thread() {
+                            public void run() {
+                                try {
+                                    daemon.close();
+                                } catch (IOException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                        });
+    }
+
+    private void startComponents() {
+        daemon.start();
+    }
+
+    private void registerInitialFiles(String[] files) {
+        for (String file : files) {
+            daemon.addFichier(file);
+        }
+    }
+
+    public static void printHelp() {
+        System.out.println("Usage : java Client <diary address> <diary port> [--no-tui]");
+    }
+
+    public static void main(String[] args) throws IOException {
+        if (args.length < 2) {
+            printHelp();
+            System.exit(1);
+        }
+
+        String host = args[0];
+        String port = args[1];
+        Boolean tui = true;
+
+        String[] files = new String[0];
+
+        // if --no-tui is passed, we don't start the TUI
+        for (int i = 2; i < args.length; i++) {
+            if (args[i].equals("--no-tui")) {
+                tui = false;
+            } else if (args[i].equals("--files")) {
+                String pwd = System.getProperty("user.dir");
+                files = args[i + 1].split(",");
+                for (int j = 0; j < files.length; j++) {
+                    files[j] = pwd + "/" + files[j];
+                }
+                i++;
+            } else if (args[i].equals("--help")) {
+                printHelp();
+                System.exit(0);
+            }
+        }
+
+        System.out.println("Starting client with Files : ");
+
+        new Client(tui, host, port, files);
     }
 }
