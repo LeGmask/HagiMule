e55b7ad2df9ea56162c5e8774b8b1ccfab8ac8a9
Evann DREUMONT <53308142+LeGmask@users.noreply.github.com>
2025-01-10 09:43:39 +0100

    fix(infra): output clients_ip

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,4 +1,9 @@
-output "instance_url" {
+output "diary_ip" {
   description = "Public IP of the diary"
   value       = aws_instance.diary.public_ip
+}
+
+output "clients_ip" {
+  description = "Public IP of the client"
+  value       = aws_instance.client.*.public_ip
 }
